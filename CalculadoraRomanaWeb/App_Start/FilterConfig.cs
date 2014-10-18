afb92fd4685c0af58bfdbc3297cd7bb8d274d7eb
Sequeiros <rafanetow@gmail.com>
2014-10-18 16:20:56 -0600

    PrimerCommit

---
+++ CalculadoraRomanaWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraRomanaWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
